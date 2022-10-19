9a5a997e4702f39caddaea01cd3392601fd4c7a4
TobiasReyEye <tobi-stoehr@t-online.de>
2022-10-19 14:20:43 +0200

    partial implemented

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -3,14 +3,13 @@ class Card (val name: String, val value: Int, val number: Int, val effect: Strin
     val cardWith = 18
     val cardHeight = 12
 
-    def fillspace(name: String): String = name + " " * (cardWith -4 - name.length)
-    def splitname (name: String): Vector[String] = {
-        if (name.length < cardWith -4) {
-            Vector(fillspace(name))
-        } else if (name.length > cardWith -4) {
-            //val tempAr = name.split("^[A-Za-z]$",12)
-            val tempVec = name.grouped(cardWith -4).toVector
-            tempVec.updated(1, fillspace(tempVec(1)))
+    def fillspace(name: String, margin: Int): String = name + " " * (cardWith -margin - name.length)
+    def splitname (name: String, margin: Int): Vector[String] = {
+        if (name.length < cardWith -margin) {
+            Vector(fillspace(name, margin))
+        } else if (name.length > cardWith -margin) {
+            val tempVec = name.grouped(cardWith -margin).toVector
+            tempVec.updated(tempVec.length - 1, fillspace(tempVec(tempVec.length - 1), margin))
         } else {
             Vector(name)
         }
@@ -18,7 +17,7 @@ class Card (val name: String, val value: Int, val number: Int, val effect: Strin
 
     def edge = "+" + "-" * cardWith + "+" + eol
     def title = {
-        val tempVec = splitname(name)
+        val tempVec = splitname(name, 4)
         val tempSt = "|" + value + " " + tempVec(0) + " " + number + "|" + eol
         if (tempVec.length == 1) {
             tempSt + "|" + (" ") * cardWith + "|" + eol
@@ -26,9 +25,25 @@ class Card (val name: String, val value: Int, val number: Int, val effect: Strin
             tempSt + "|  " + tempVec(1) + "  |" + eol
         }
     }
-    def body = ("|" + ("H") * cardWith + "|" + eol) * cardHeight
+    def body = {
+        //("|" + ("H") * cardWith + "|" + eol) * cardHeight
+        val tempVec = splitname(effect, 2)
+        //val tempSt = "Hallo"
+        val tempArray: Array[String] = tempVec.toArray
+        for (i <- 0 until cardHeight - 1) {
+            if (tempVec.length > i) {
+                tempArray.updated(0, tempArray(0) + "|" + tempVec(i) + "|" + eol)
+                //tempSt.appended("|" + tempVec(i) + "|" + eol)
+            } else {
+                //tempSt.appended("|" + ("H") * cardWith + "|" + eol)
+                tempArray.updated(0, tempArray(0) + "|" + ("H") * cardWith + "|" + eol)
+            }
+        }
+        //tempSt
+        tempArray(0)
+    }
 
     def card = edge + title + body + edge
 
-  override def toString: String = card
+    override def toString: String = card
 }
