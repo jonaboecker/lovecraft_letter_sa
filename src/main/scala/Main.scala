1eee4e524b90b60ef4b6eeb624d8b666049ec8e9
jbcker <jona.boecker@web.de>
2022-10-21 09:38:12 +0200

    add Card Tiefe Wesen

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,6 +14,7 @@
         "Wenn du den Gehirnzylinder ablegst, scheidest du aus"
     )
     val card3 = new Card("Investigator2", 1, 5, "Rate", "")
+    val card4 = new Card("Tiefe Wesen", 1, 1, "Errätst du den Wert der Hand karte eines Mitspielers (außer der 1), scheidet dieser aus.", "Wähle einen Mitspieler Besitzt dessen Handkarte eine scheidet er aus. Wenn nicht, wende die normale Funktion dieser Karte gegen den Mitspieler an.")
     // println(card)
     // println(card2)
     var tempVec = Vector(card1, card2, card3)
