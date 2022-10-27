0a4a86b217fbbc1144bb8460dfd37aafe631f167
TobiasReyEye <tobi-stoehr@t-online.de>
2022-10-27 17:04:15 +0200

    added BoardSpec.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,36 @@
+package scala
 //val eol = sys.props("line.separator")
 //def bar() = (("+" + "-" * 5) * 5) + "+" + eol
 
 @main def hello: Unit =
-    println("Hello world!!")
-    println(msg)
-    // println(bar())
-    val card1 = new Card("Investigator", 1, 5, "Rate", "")
-    val card2 = new Card(
-        "Gehirnzylinder der MI-GO",
-        0,
-        1,
-        "Wenn du den Gehirnzylinder ablegst, scheidest du aus",
-        "Wenn du den Gehirnzylinder ablegst, scheidest du aus"
-    )
-    val card3 = new Card("Der leuchtende Trapezoeder", 7, 1, "Wenn du zusätzlich eine Karte mit einer \"5\" oder höher auf der Hand hast, musst du den leuchtenden Trapezoeder ausspielen. Ignoriere dies, wenn du wahnsinnig bist.", "Wenn du zusätzlich eine Karte mit einer \"5\" oder höher auf der Hand hast, gewinnst du die Runde.")
-    val card4 = new Card("Tiefe Wesen", 1, 1, "Errätst du den Wert der Hand karte eines Mitspielers (außer der 1), scheidet dieser aus.", "Wähle einen Mitspieler Besitzt dessen Handkarte eine scheidet er aus. Wenn nicht, wende die normale Funktion dieser Karte gegen den Mitspieler an.")
-    // println(card)
-    // println(card2)
-    var tempVec = Vector(card1, card2, card3)
-    print(new Board(tempVec))
+  println("Hello world!!")
+  println(msg)
+  // println(bar())
+  val card1 = new Card("Investigator", 1, 5, "Rate", "")
+  val card2 = new Card(
+    "Gehirnzylinder der MI-GO",
+    0,
+    1,
+    "Wenn du den Gehirnzylinder ablegst, scheidest du aus",
+    "Wenn du den Gehirnzylinder ablegst, scheidest du aus"
+  )
+  val card3 = new Card(
+    "Der leuchtende Trapezoeder",
+    7,
+    1,
+    "Wenn du zusätzlich eine Karte mit einer \"5\" oder höher auf der Hand hast, musst du den leuchtenden Trapezoeder ausspielen. Ignoriere dies, wenn du wahnsinnig bist.",
+    "Wenn du zusätzlich eine Karte mit einer \"5\" oder höher auf der Hand hast, gewinnst du die Runde."
+  )
+  val card4 = new Card(
+    "Tiefe Wesen",
+    1,
+    1,
+    "Errätst du den Wert der Hand karte eines Mitspielers (außer der 1), scheidet dieser aus.",
+    "Wähle einen Mitspieler Besitzt dessen Handkarte eine scheidet er aus. Wenn nicht, wende die normale Funktion dieser Karte gegen den Mitspieler an."
+  )
+  // println(card)
+  // println(card2)
+  val tempVec = Vector(card1, card2, card3)
+  print(new Board(tempVec))
 
 def msg = "I was compiled by Scala 3. :)"
---
+++ src/test/scala/BoardSpec.scala
@@ -0,0 +1,46 @@
+package scala
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest._
+
+class BoardSpec extends AnyWordSpec with Matchers {
+
+  val eol = sys.props("line.separator")
+
+  "A Board" should {
+    // val defaultBoard = new Board(1, 2, 3)
+
+    "return the correct String" in {
+      val defaultBoard = new Board(Vector(1, 1, 1))
+      val EdgeResult =
+        ("+------------------+   +------------------+   +------------------+" + eol)
+      defaultBoard.edge() should ===(EdgeResult)
+      val TitelResult = (
+        "|1 Investigator   5|   |1 Investigator   5|   |1 Investigator   5|" + eol +
+          "|                  |   |                  |   |                  |" + eol
+      )
+      defaultBoard.titel() should ===(TitelResult)
+      val BodyResult = (
+        "| Rate             |   | Rate             |   | Rate             |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol +
+          "|                  |   |                  |   |                  |" + eol
+      )
+      defaultBoard.body() should ===(BodyResult)
+
+      defaultBoard.toString() should ===(
+        EdgeResult + TitelResult + BodyResult + EdgeResult
+      )
+    }
+
+  }
+}
