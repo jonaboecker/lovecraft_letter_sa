4126753106996a49ed23c2e1e2620324ff9cc96b
TobiasReyEye <tobi-stoehr@t-online.de>
2022-12-01 18:02:02 +0100

    added PlayCommand

---
+++ src/main/scala/de/htwg/lovecraftletter/controller/PlayCommand.scala
@@ -0,0 +1,11 @@
+package de.htwg.lovecraftletter
+package controller
+
+import util._
+import model._
+
+class PlayCommand(contr: Controller) extends Command[GameState]{
+    def doStep(state: GameState): GameState = contr.playCard
+    def undoStep(state: GameState): GameState = contr.undoStep
+    def redoStep(state: GameState): GameState = contr.redoStep
+}
