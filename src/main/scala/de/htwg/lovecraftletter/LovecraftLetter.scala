98cb1d40e337fc5d0587d05e916388d1d9d84341
jonaboecker <jona.boecker@web.de>
2023-10-10 16:50:51 +0200

    cleanup main

--- src/main/scala/de/htwg/lovecraftletter/LovecraftLetter.scala
+++ src/main/scala/de/htwg/lovecraftletter/LovecraftLetter.scala
@@ -1,17 +1,8 @@
 package de.htwg.lovecraftletter
 
 import aview._
-import model.GameStateInterface
-import model.GameStateImpl._
-import controller.ControllerInterface
-import controller.controllState
-import controller.controllerImpl._
 import de.htwg.lovecraftletter.LovecraftLetterModule.given_ControllerInterface
 
-
-
-//val eol = sys.props("line.separator")
-//def bar() = (("+" + "-" * 5) * 5) + "+" + eol
 
 @main def run: Unit = {
   println("Welcome to LovecraftLetter")
